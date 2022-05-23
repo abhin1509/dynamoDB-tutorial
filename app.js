90b53ccbfb5f78e15849d461b909ef0eeff32df5
abhin1509 <abhin1509@gmail.com>
2022-05-23 12:05:28 +0530

    updated the code

--- app.js
+++ app.js
@@ -1,53 +1,73 @@
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+const sendResponse = (code, bool, msg, list) => {
+  return {
+    statusCode: code,
+    body: JSON.stringify({
+      success: bool,
+      message: msg,
+      items: list
+    })
+  }
+}
+
 const getData = async (id) => {
-  return await dynamoDB.get({
+  const res = await dynamoDB.get({
     TableName: "todoTable",
     Key: { id },
   }).promise();
+  return sendResponse(200, true, 'data fetched successfully', res.Item);
 }
 
-const createData = async ({ id, newName }) => {
+const createData = async ({ id, title }) => {
   await dynamoDB.put({
-    Item: { id, newName }, //caution:: don't pass obj directly
+    Item: { id, title },
     TableName: "todoTable",
   }).promise();
+  return sendResponse(200, true, 'data created successfully', { id, title });
 }
 
-const updateData = async ({ id, newName }) => {
- const res = await dynamoDB
+const updateData = async ({id, title}) => {
+  const res = await dynamoDB
     .update({
       TableName: "todoTable",
       Key: { id },
-      UpdateExpression: `set newName = :x`,
+      UpdateExpression: `set title = :x`,
       ExpressionAttributeValues: {
-        ":x": newName,
+        ":x": title,
       },
-      ReturnValues: "UPDATED_OLD"
+      ReturnValues: "UPDATED_NEW",
     }).promise();
-  console.log(res);
-  return res;
+  console.log("res:: " + res);
+  return sendResponse(200, true, 'Updated successfully', res);
 }
 
 const deleteData = async (id) => {
   await dynamoDB.delete({
-      TableName: "todoTable",
-      Key: { id },
-    }).promise();
+    TableName: "todoTable",
+    Key: { id },
+  }).promise();
+  return sendResponse(200, true, 'deleted successfully', { id });
 }
 
 exports.handler = async (event) => {
-  let method = event.method;
+  let { id, title, } = event.queryStringParameters || {};
+  let method = event.httpMethod;
   try {
-    switch (method) {
-      case 1: return getData(event.id);
-      case 2: return createData(event);
-      case 3: return updateData(event);
-      case 4: return deleteData(event.id);
+    switch (true) {
+      case method === 'GET' && event.path === '/list':
+        return getData(id);
+      case method === 'POST' && event.path === '/list':
+        return createData(JSON.parse(event.body));
+      case method === 'PUT' && event.path === '/list':
+        return updateData(JSON.parse(event.body));
+      case method === 'DELETE' && event.path === '/list':
+        return deleteData(id);
     }
   }
   catch (e) {
-    console.log(e);
+    return sendResponse(500, false, 'error', e);
+    //console.log(e);
   }
 }
